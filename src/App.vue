43e6dc8acdeccdc42620e6367e3d81f607efa700
zyf <970695936@qq.com>
2021-01-09 11:22:19 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>vue admin</h1>
+    <button @click="test">点击</button>
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      a: 1,
+      b: '2'
+    }
+  },
+  methods: {
+    test (): string {
+      return this.b
+    }
+  }
+})
+</script>
+
+<style lang="scss"></style>
